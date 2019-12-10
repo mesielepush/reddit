7790a0f40576798d03b8da164a3cc0a27e3f7e13
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-12-10 15:24:51 -0600

    finished I guess

--- reddit/app/models/comment.rb
+++ reddit/app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
-  
   belongs_to :user, foreign_key: 'user_id'
   belongs_to :post, foreign_key: 'post_id'
   validates :body, presence: true, length: { minimum: 3 }
