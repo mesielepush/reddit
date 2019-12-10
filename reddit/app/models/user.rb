a9c88fe1a34b00abdfdc54b76a782d5f3632b941
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-12-10 15:41:57 -0600

    changes by rubocop

--- reddit/app/models/user.rb
+++ reddit/app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
