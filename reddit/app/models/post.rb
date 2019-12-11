533402d8d78e160c7af707d4898ce583f7818cdf
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-12-11 11:53:21 -0600

    Update post.rb

--- reddit/app/models/post.rb
+++ reddit/app/models/post.rb
@@ -3,6 +3,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true
-  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :user, foreign_key: 'user_id'
   has_many :comments
 end
